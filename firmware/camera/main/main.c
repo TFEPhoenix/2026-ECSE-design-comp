7f47e216460ff048d3c07f7ea21b559cec272158
FusionSid <siddheshadsv@icloud.com>
2026-07-25 23:12:04 +1200

    fix: use actual uart pins for sending data

--- firmware/camera/main/main.c
+++ firmware/camera/main/main.c
@@ -1,11 +1,23 @@
 #include "blob.h"
 #include "camera_config.h"
-#include "homography.h"
-
+#include "driver/uart.h"
 #include "esp_camera.h"
 #include "freertos/FreeRTOS.h"
+#include "homography.h"
+#include <string.h>
+
+#define UART_TX 13
+#define UART_RX 12
+#define UART_PORT UART_NUM_1
+#define BUF_SIZE 1024
+
+bool init_uart();
 
 void app_main(void) {
+    if (!init_uart()) {
+        return;
+    }
+
     if (camera_init() != ESP_OK) {
         return;
     }
@@ -30,7 +42,29 @@ void app_main(void) {
             position = apply_homography(&h_inv, center);
         }
 
-        printf("POS,%.4f,%.4f,%d\n", position.col, position.row, found ? 1 : 0);
+        char message[256];
+        sprintf(message, "POS,%.4f,%.4f,%d\n", position.col, position.row,
+                found ? 1 : 0);
+
+        uart_write_bytes(UART_PORT, message, strlen(message));
         esp_camera_fb_return(fb);
     }
+}
+
+bool init_uart() {
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    };
+    int intr_alloc_flags = 0;
+
+    esp_err_t e1 =
+        uart_driver_install(UART_PORT, BUF_SIZE, 0, 0, NULL, intr_alloc_flags);
+    esp_err_t e2 = uart_param_config(UART_PORT, &uart_config);
+    esp_err_t e3 = uart_set_pin(UART_PORT, UART_TX, UART_RX, -1, -1);
+
+    return e1 == ESP_OK && e2 == ESP_OK && e3 == ESP_OK;
 }
